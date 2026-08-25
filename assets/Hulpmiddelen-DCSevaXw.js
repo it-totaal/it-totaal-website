69fec44c76943f72f139dfb959fda56d0235ff76
Patrick Luisman <patrickluisman@MacBook-Pro-van-Patrick.local>
2026-08-25 15:55:57 +0200

    Updates

--- assets/Hulpmiddelen-DCSevaXw.js
+++ assets/Hulpmiddelen-DCSevaXw.js
@@ -1,4 +1,4 @@
-import{c as l,j as e,S as c,C as d}from"./index-BBz6kLXq.js";import{F as a}from"./file-text-DrFldBHg.js";import{L as n}from"./react-vendor-BvDay7C4.js";import"./supabase-vendor-D7HrI6pR.js";/**
+import{c as l,j as e,S as c,C as d}from"./index-CBq75cuX.js";import{F as a}from"./file-text-B2Kvzl-K.js";import{L as n}from"./react-vendor-BvDay7C4.js";import"./supabase-vendor-D7HrI6pR.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
