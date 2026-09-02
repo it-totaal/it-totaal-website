685c7210263065e10a432eb4231cd50acf4a6db3
Patrick Luisman <patrickluisman@macbook-pro-van-patrick.tail63aac5.ts.net>
2026-09-02 14:29:56 +0200

    Updates

--- assets/CustomerMap-C08j_Rvm.js
+++ assets/CustomerMap-C08j_Rvm.js
@@ -1,4 +1,4 @@
-import{j as t,f as x}from"./index-B5ze-Rfi.js";import{r}from"./react-vendor-BvDay7C4.js";import{M as g,T as b,a as w,L as c}from"./map-vendor-J-BAuCJ2.js";const v=()=>{const l=`
+import{j as t,f as x}from"./index-BOoz5tsk.js";import{r}from"./react-vendor-BvDay7C4.js";import{M as g,T as b,a as w,L as c}from"./map-vendor-J-BAuCJ2.js";const v=()=>{const l=`
     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="24" cy="24" r="20" fill="#46f0a4" opacity="0.2"/>
       <circle cx="24" cy="24" r="5" fill="#46f0a4"/>
